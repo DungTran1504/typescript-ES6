f2b8d60dd471c802177b0ce49cffebf531be0796
unknown <dungtran1504@gmail.com>
2018-06-03 11:30:46 +0700

    lesson16.ts

---
+++ lesson16.ts
@@ -0,0 +1,16 @@
+//vòng lặp for
+var colorName=['Red','Green','Blue'];
+for(var i in colorName){
+    console.log(colorName[i]);
+}
+
+//vòng lặp for of
+for(var item of colorName){
+    console.log(item);
+}
+
+//vòng lặp ký tự
+var sentence="Dung Tran"
+for(var i of sentence){
+    console.log(i);
+}
