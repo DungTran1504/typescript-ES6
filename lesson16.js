c40cdeeb985357c802a5b56c497f2984eea28011
unknown <dungtran1504@gmail.com>
2018-06-03 11:14:03 +0700

    some message

---
+++ lesson16.js
@@ -0,0 +1,17 @@
+"use strict";
+//vòng lặp for 
+var colorName = ['Red', 'Green', 'Blue'];
+for (var i in colorName) {
+    console.log(colorName[i]);
+}
+//vòng lặp for of
+for (var _i = 0, colorName_1 = colorName; _i < colorName_1.length; _i++) {
+    var item = colorName_1[_i];
+    console.log(item);
+}
+//lặp kí tự
+var sentence = 'Thu Dung';
+for (var _a = 0, sentence_1 = sentence; _a < sentence_1.length; _a++) {
+    var item = sentence_1[_a];
+    console.log(item);
+}
